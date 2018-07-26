d1d5cc62e71a4017ee7cec03a3cc75c2956eda37
Bryan Pinto <bryaan.pintoo@gmail.com>
2018-07-26 19:44:21 -0400

    no pude terminarlo, me falto tiempo

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,23 @@ $(document).ready(function(){
 			$(".body__post").on('submit', function(event){
 				var text = $("#texto").val();
 				event.preventDefault();
-				$(".body__posteos2").append('<div class="row__post">'+
+				$(".body__posteos2").append('<div class="body__posteos2">'+
+					'<div class="row__post">'+
 					'<div class="img__profile-post2">'+
+					'<img class="img__profile" src="./assets/images/profile.png" alt="">' +
 					'</div>' +
-					'<div class="content__post">' +
-					'<h1>Likes</h1>' +
-					'<span id="counter">0</span>' +
-					'</div>' +
+					'<div class="content__post">' +	
+					'<div class="content__text">' +		
 					text +
-					'<a class="likes" href="#"><i class="fas fa-heart fa-2x"></i></a>'+
+					'</div>'+
+					'<div class="content__likes">' +
+					'<a class="likes" href="#"><i class="fas fa-heart fa-2x"></i></a>'+					
+					'<span id="counter">0</span>' +	
+					'</div>' +
+					'</div>' +
 					'<div class="delete__post">' +
 					'<i class="fas fa-trash-alt fa-2x"></i>' +
+					'</div>' +
 					'</div>' +
 					'</div>')
 				$("#texto").val("");
@@ -21,13 +27,16 @@ $(document).ready(function(){
 //borrar tweets
 $(".body__posteos2").on("click", ".delete__post", function(event){
 	event.stopPropagation();
-	$(this).parent().fadeOut(500);
+	$(this).parent().parent().fadeOut(500);
+});
+
+$(".likes").on("click", function(){
+	$(this).addClass("likes__red").removeAttr("href");
 });
 
 $(".likes").on("click", function(){
 	 counter = $("#counter").text();
 	 counter = parseInt(counter);
-	 console.log(counter + 1);
 	 $("#counter").text(counter + 1);
 	});
 
